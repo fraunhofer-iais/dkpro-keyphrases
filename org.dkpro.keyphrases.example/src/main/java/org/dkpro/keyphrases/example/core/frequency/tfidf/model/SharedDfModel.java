3f89cf24376673bab42b4bc50391928e61e043f7
Andreas Steenpass <andreas.steenpass@iais.fraunhofer.de>
2019-11-08 13:03:11 +0100

    change package names in copied files

--- org.dkpro.keyphrases.example/src/main/java/org/dkpro/keyphrases/example/core/frequency/tfidf/model/SharedDfModel.java
+++ org.dkpro.keyphrases.example/src/main/java/org/dkpro/keyphrases/example/core/frequency/tfidf/model/SharedDfModel.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.model;
+package org.dkpro.keyphrases.example.core.frequency.tfidf.model;
 
 import static org.apache.uima.fit.factory.ExternalResourceFactory.bindResource;
 
